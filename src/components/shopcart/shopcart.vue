ac10499b9e7372d3e3cdb2dc9cb76895052d497f
jack <36956271@qq.com>
2019-11-11 20:45:43 +0800

    2019/11/11

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="shop-cart">123</div>
+</template>
+
+<script>
+  export default {
+    name: 'shopcart',
+    props: {
+      selectFoods: {
+        type: Array,
+        default () {
+          return []
+        },
+        deliveryPrice: {type: Number, default: 0},
+        minPrice: {type: Number, default: 0}
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylussheet/stylus">
+
+</style>
